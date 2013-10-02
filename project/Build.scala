a4b20590c05e47d51e3bba045bc945c6749e3ccf
Lauri Fjällström <lauri.fjallstrom@intoloop.com>
2013-10-02 20:06:38 +0300

    Created project

---
+++ project/Build.scala
@@ -0,0 +1,25 @@
+import sbt._
+import Keys._
+
+object ValidationBuild extends Build {
+
+  val librarySettings = Seq(
+  	organization := "com.github.lfjallstrom",
+    version := "0.0.1",
+    scalaVersion := "2.10.3"
+  )
+
+  val dependencies = Seq(
+  	"org.scalatest" %% "scalatest" % "1.9.2" % "test"
+  )
+
+  val sources = Seq(
+    unmanagedSourceDirectories in Compile := (scalaSource in Compile).value :: Nil,
+    unmanagedSourceDirectories in Test := (scalaSource in Test).value :: Nil
+  )
+
+  val project = Project("validation", file("."))
+    .settings(librarySettings:_*)
+    .settings(sources:_*)
+    .settings(libraryDependencies ++= dependencies)
+}
